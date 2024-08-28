538b34f4462ef436a51a7871748e9e1ba0494add
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-28 15:20:37 +0545

    Sport Timer

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import {useState} from "react";
+
+function App() {
+  const [time, setTime] = useState[0];
+  const [running, setRunning] = useState(false);
+  return(
+    <>
+      <h1>01- stopwatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60))}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60))}:</span>
+        <span>{("0" + ((time / 10) % 100))}</span>
+
+      </div>
+      <div>
+        <button onClick={()=>{setRunning(true)}}>Start</button>
+        <button onClick={()=>{setRunning(false)}}>Stop</button>
+        <button onClick={()=>{setTime(0)}}>Reset</button>
+      </div>
+    </>
+  )
+}
+
+export default App
